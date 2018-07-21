1b88e9176bab3e9d7e20f80d390e9fca3dc7ab7c
Courtney Meyerhofer <meyerhoferc@gmail.com>
2018-07-21 13:39:38 -0700

    Add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'welcome#index'
+  # note Rails has a helper for the above "root" path: root to: 'welcome#index'
+  # to see all routes and connections to controllers, run `rails routes` Rake task
 end

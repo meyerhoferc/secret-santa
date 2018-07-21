fec8c7259d6d65b9e01a0ad30ccb4961a944f47b
Courtney Meyerhofer <meyerhoferc@gmail.com>
2018-07-21 13:20:31 -0700

    Add user seeds with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create!(first_name: Faker::LordOfTheRings.character,
+               last_name: Faker::LordOfTheRings.character,
+               email: Faker::SiliconValley.email,
+               password: Faker::LordOfTheRings.location)
+end

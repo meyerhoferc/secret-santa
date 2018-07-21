cb54483e397e0499094580c760449de9b407d752
kmeyerhofer <k@kcmr.io>
2018-07-21 15:56:26 -0700

    Changes homepage to display all users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: 'welcome#index'
+  # get '/', to: 'welcome#index'
+  root to: 'welcome#index'
   # note Rails has a helper for the above "root" path: root to: 'welcome#index'
   # to see all routes and connections to controllers, run `rails routes` Rake task
 end

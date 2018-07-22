f2a4ba6dca0239f913fe006881d2239700f16b63
kmeyerhofer <k@kcmr.io>
2018-07-21 18:56:02 -0700

    Adds user creation and login pages. DB connection exceptions raising.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  attr_accessor :email, :first_name, :last_name,
+                  :password, :password_confirmation
+
+  validates_uniqueness_of :email
 end

e29d994cef6286fe878286bbe4912f1e0411ade7
Kurt <k@kcmr.io>
2018-08-17 19:29:50 -0700

    Adds user model validation tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,27 @@ describe User do
     expect(u.authenticate('notthepassword')).to be_falsey
     expect(u.authenticate('password123')).to eq u
   end
+
+  describe 'validations' do
+    it 'validates email is present' do
+      u = User.new(first_name: 'Raz', last_name: 'Z', email: '', password: 'passpass')
+      expect(u.valid?).to eq false
+    end
+
+    it 'validates first name is present' do
+      u = User.new(first_name: nil, last_name: 'Z', email: 'email@email.email', password: 'passpass')
+      expect(u.valid?).to be_falsey
+    end
+
+    it 'validates last name is present' do
+      u = User.new(first_name: 'Razz', last_name: nil, email: 'email@email.email', password: 'passpass')
+      expect(u.valid?).to be_falsey
+    end
+
+    it 'validates email is unique' do
+      User.create(first_name: 'Raz', last_name: 'Z', email: 'email@email.email', password: 'passpass')
+      v = User.new(first_name: 'Raz', last_name: 'Z', email: 'email@email.email', password: 'passpass')
+      expect(v.valid?).to eq false
+    end
+  end
 end

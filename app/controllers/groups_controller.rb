d6a8863db62c01081369e0cc75a24efbdb043801
Kurt <k@kcmr.io>
2018-10-01 16:58:53 -0600

    Adds update failure check, flash message project convention, listing of users' wish lists on #show page.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,11 +12,12 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user_ids = current_user.id # Add user as a user
     @group.owner_id = current_user.id # Add user as owner
+    # CREATE THE WISHLIST HERE! SET IT AS THE OWNER'S LIST
     if @group.save
-      redirect_to group_path(@group.id) # Find the group page
+      redirect_to group_path(@group) # Find the group page
     else
       flash[:notice] = 'The group name is already taken. Please choose another name.'
-      render 'new'
+      redirect_to new_group_path(@group)
     end
   end
 
@@ -29,14 +30,19 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group.update(group_params)
-    flash.notice = "Group '#{@group.name}' updated!"
-    redirect_to group_path(@group)
+    if @group.update(group_params)
+      flash[:notice] = "Group '#{@group.name}' updated!"
+      redirect_to group_path(@group)
+    else
+      flash[:warning] = 'An error occurred, please try again.'
+      redirect_to edit_group_path(@group)
+    end
   end
 
   def destroy
+    # Destroy not working. Delete lists associated with group first.
     @group.destroy
-    flash.notice = "Group Deleted!"
+    flash[:notice] = "Group Deleted!"
     redirect_to dashboard_path
   end
 

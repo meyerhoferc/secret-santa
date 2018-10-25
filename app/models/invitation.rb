afafd601444b2abddc25deb6bb123cebc24356c4
Kurt Meyerhofer <k@kcmr.io>
2018-10-24 19:00:31 -0600

    Removes comment.

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,5 +3,4 @@ class Invitation < ApplicationRecord
   belongs_to :user, foreign_key: :receiver_id
   belongs_to :group
   validates :comment, presence: true
-  # validates :accepted, :inclusion => {:in => [true, false]}
 end

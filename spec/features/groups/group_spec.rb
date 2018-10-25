2be5ab691faa51b4ed0f7d0571f3f47e0c27aaf2
Kurt Meyerhofer <k@kcmr.io>
2018-10-24 19:02:36 -0600

    These tests now pass.

--- spec/features/groups/group_spec.rb
+++ spec/features/groups/group_spec.rb
@@ -1,18 +1,32 @@
 require 'rails_helper'
 
-xdescribe 'groups' do
+describe 'group creation' do
   before { @user = User.create(first_name: 'Raa', last_name: 'Zzz', email: 'email@raa.zzz', password: 'passpass') }
-  context 'are created' do
+  context 'created' do
     it 'correctly' do
-      @group_info = { name: 'This sdfis one adwesome grfoup', description: 'What a description' }
+      @group_info = { name: 'This is one awesome group!',
+                      description: 'What a description',
+                      due_date: '2019-02-28' }
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       visit new_group_path
       fill_in('group_name', with: @group_info[:name])
       fill_in('group_description', with: @group_info[:description])
+      fill_in('group_gift_due_date', with: @group_info[:due_date])
       click_on 'Create Group'
+
       expect(page).to have_content "#{@group_info[:name]}"
       expect(page).to have_content "#{@group_info[:description]}"
+      expect(page).to have_content "#{@group_info[:due_date]}"
+    end
 
+    it 'incorrectly' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit new_group_path
+      fill_in('group_name', with: '')
+      fill_in('group_description', with: '')
+      click_on 'Create Group'
+
+      expect(page).to have_content "The Gift Due Date can't be blank, or the Group Name is already taken."
     end
   end
 end

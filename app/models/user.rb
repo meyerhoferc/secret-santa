35a052e1bd44f44446540104340a3ef867abe57d
Kurt Meyerhofer <k@kcmr.io>
2018-10-26 17:04:09 -0600

    Adds strong password validators.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
+
   has_many :user_groups
   has_many :groups, through: :user_groups
   has_many :lists
   has_many :invitations
-  has_secure_password
   validates_uniqueness_of :email
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+
+  include ActiveModel::Validations
+  has_secure_password
+  validates :password, password_strength: {use_dictionary: true}
 end

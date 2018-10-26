7c4c99a187b76dbb78467279f1bfe8b5b4f002b2
Kurt Meyerhofer <k@kcmr.io>
2018-10-26 11:04:27 -0600

    Adds feature tests for item creation, editing and deletion.

---
+++ spec/features/items/deleting_item_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'deleting' do
+  let(:user) { User.create(first_name: 'Raaa', last_name: 'Zzzz', email: 'emmaiil@raa.zzz', password: 'passpass') }
+  let(:group) { Group.create(name: 'The best group there is, that I know', description: 'Whoever wants to join', gift_due_date: '2019/12/31') }
+  let(:item_one) { Item.create(name: 'Keys', description: 'So many', size: 'S', note: 'I have many of these.') }
+
+  it 'an item' do
+    sign_in_as(user)
+    click_on 'Create a Group'
+    create_group(group)
+    click_on 'Create Group'
+    visit dashboard_path
+    click_on group.name
+    click_on 'View your Wish List'
+    click_on 'Create a new Wish list item'
+    create_item(item_one)
+    click_on 'Create Item'
+
+    expect(page).to have_link item_one.name
+    click_on item_one.name
+    click_on 'Delete Item'
+
+    expect(page).to have_content "Item, #{item_one.name}, Deleted!"
+    expect(page).to have_no_content item_one.description
+    expect(page).to have_no_content item_one.size
+    expect(page).to have_no_content item_one.note
+  end
+end
---
+++ spec/features/items/editing_item_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+describe 'editing an item' do
+  context 'with' do
+    let(:user) { User.create(first_name: 'Raa', last_name: 'Zzz', email: 'emaiil@raa.zzz', password: 'passpass') }
+    let(:group) { Group.create(name: 'The best group there is', description: 'Whoever wants to join', gift_due_date: '2018/12/31') }
+    let(:item_one) { Item.create(name: 'Wallet', description: 'So pretty', size: 'XL', note: 'I would like many of these.') }
+    let(:item_two) { Item.create(name: 'Shoes', description: 'So pretty!', size: 'L', note: 'I would like many of these please.') }
+
+    it 'correct information' do
+      sign_in_as(user)
+      click_on 'Create a Group'
+      create_group(group)
+      click_on 'Create Group'
+      visit dashboard_path
+      click_on group.name
+      click_on 'View your Wish List'
+      click_on 'Create a new Wish list item'
+      create_item(item_one)
+      click_on 'Create Item'
+
+      expect(page).to have_link item_one.name
+      click_on item_one.name
+      click_on 'Edit Item'
+
+      edit_item(item_one)
+      click_on 'Update Item'
+
+      expect(page).to have_content item_one.name + '!!'
+      expect(page).to have_content item_one.description + '!!'
+      expect(page).to have_content item_one.size + '!!'
+      expect(page).to have_content item_one.note + '!!'
+    end
+
+    it 'blank information' do
+      sign_in_as(user)
+      click_on 'Create a Group'
+      create_group(group)
+      click_on 'Create Group'
+      visit dashboard_path
+      click_on group.name
+      click_on 'View your Wish List'
+      click_on 'Create a new Wish list item'
+      click_on 'Create Item'
+
+      expect(page).to have_content 'Please enter valid information.'
+    end
+
+    it 'multiple items' do
+      sign_in_as(user)
+      click_on 'Create a Group'
+      create_group(group)
+      click_on 'Create Group'
+      visit dashboard_path
+      click_on group.name
+      click_on 'View your Wish List'
+      click_on 'Create a new Wish list item'
+      create_item(item_one)
+      click_on 'Create Item'
+
+      expect(page).to have_link item_one.name
+      click_on 'Create a new Wish list item'
+      create_item(item_two)
+      click_on 'Create Item'
+
+      expect(page).to have_link item_two.name
+      click_on item_one.name
+      item_page_content(item_one)
+
+      click_on 'Back to List'
+      click_on item_two.name
+      item_page_content(item_two)
+    end
+  end
+end

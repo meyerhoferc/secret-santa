bc6d6b8414be07c96eda21608b94451dddd08de3
Kurt Meyerhofer <k@kcmr.io>
2018-10-27 17:31:54 -0600

    Adds unique validation to username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ApplicationRecord
+  include ActiveModel::Validations
 
   has_many :user_groups
   has_many :groups, through: :user_groups
   has_many :lists
   has_many :invitations
-  validates_uniqueness_of :email
+
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :username, presence: true
+  validates_uniqueness_of :username
   validates :email, presence: true
-
-  include ActiveModel::Validations
+  validates_uniqueness_of :email
   has_secure_password
   validates :password, password_strength: {use_dictionary: true}
 end

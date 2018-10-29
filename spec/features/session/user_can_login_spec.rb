70aec567bc256a48f6b56dca52c5ff1397f9f161
Kurt Meyerhofer <k@kcmr.io>
2018-10-28 19:59:49 -0600

    Adds feature tests for signing in with username.

--- spec/features/session/user_can_login_spec.rb
+++ spec/features/session/user_can_login_spec.rb
@@ -1,14 +1,9 @@
 require 'rails_helper'
 
 describe "user login" do
-  # Examples
-  # before { @user = User.create() }
-  # let(:user) { User.create() }
-  # let!(:user) { User.create() }
-  # before { @user = User.create(first_name: 'Test', last_name: 'Ing', email: 'test@ing.com', password: 'password') }
   let(:user) { User.create(first_name: 'Test', last_name: 'Ing', username: 'ingTest', email: 'test@ing.com', password: 'pas1203489y132809hswo1203489y132809hrd') }
-  context 'with correct login information' do
-    it 'can login' do
+  context 'with email' do
+    it 'correctly' do
       visit new_session_path
       fill_in('email', with: user.email)
       fill_in('password', with: user.password)
@@ -17,26 +12,56 @@ describe "user login" do
       expect(current_path).to eq dashboard_path
       expect(page).to have_content 'Welcome, Test.'
     end
-    it 'but wrong case' do
+
+    it 'correctly but wrong case' do
       visit new_session_path
-      fill_in('email', with: user.email.upcase)
+      fill_in('username_email', with: user.email.upcase)
       fill_in('password', with: user.password)
       click_on 'Log In'
 
       expect(current_path). to eq dashboard_path
       expect(page).to have_content 'Welcome, Test.'
     end
-  end
-  context 'with incorrect login information' do
-    it 'rejects login' do
+
+    it 'incorrect password' do
       visit new_session_path
       fill_in('email', with: user.email)
       fill_in('password', with: user.first_name)
       click_on 'Log In'
 
       expect(current_path).to eq sessions_path
-      expect(page).to have_content 'Email or password is invalid'
+      expect(page).to have_content 'Username, Email or password invalid.'
       expect(page).to have_link 'Back to Homepage'
+    end
+  end
+
+  context 'with username' do
+    let(:user) { User.create(first_name: 'Test', last_name: 'Ing', username: 'ingtest', password: 'pas1203489y132809hswo1203489y132809hrd') }
+    it 'correctly' do
+      sign_in_username(user)
+
+      expect(page).to have_content 'Welcome, Test.'
+      expect(current_path).to eq dashboard_path
+    end
+
+    it 'correctly but wrong case' do
+      visit login_path
+      fill_in 'Username or email', with: user.username.upcase
+      fill_in 'Password', with: user.password
+      click_on 'Log In'
+
+      expect(page).to have_content 'Welcome, Test.'
+      expect(current_path).to eq dashboard_path
+    end
+
+
+    it 'incorrect password' do
+      visit login_path
+      fill_in 'Username or email', with: user.username.upcase
+      fill_in 'Password', with: user.password + '6'
+      click_on 'Log In'
+
+      expect(page).to have_content 'Username, Email or password invalid.'
     end
   end
 end

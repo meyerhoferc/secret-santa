6459462c9c5761d3b2c3c59d44b42e56fa4a6228
Kurt Meyerhofer <k@kcmr.io>
2018-10-29 16:50:30 -0600

    Adds and fixes feature tests to invite a user to a group with their username.

--- spec/features/invitations/creating_invitation_spec.rb
+++ spec/features/invitations/creating_invitation_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'creating an invitation' do
   let(:group) { Group.create(name: 'Gift giving', description: 'We love gifts', gift_due_date: '2019-01-01') }
-  let(:owner) { User.create(first_name: 'Ray', last_name: 'Lee', username: 'LeeRay', email: 'email@ray.lee', password: 'hspa1203489y132809hss12034pas1203489y13280989y132809h') }
-  let(:invitee) { User.create!(first_name: 'Lee', last_name: 'Ray', username: 'RayLee', email: 'lee@ray.com', password: 'pas1203489y132809h132809hss1203489y132spa1203489y809h') }
+  let(:owner) { User.create(first_name: 'Ray', last_name: 'Lee', username: 'leeray', email: 'email@ray.lee', password: 'hspa1203489y132809hss12034pas1203489y13280989y132809h') }
+  let(:invitee) { User.create!(first_name: 'Lee', last_name: 'Ray', username: 'raylee', email: 'lee@ray.com', password: 'pas1203489y132809h132809hss1203489y132spa1203489y809h') }
 
   context 'as a group owner' do
     it 'inviting from user profile' do
@@ -30,37 +30,81 @@ describe 'creating an invitation' do
       expect(page).to have_no_content "Invite #{owner.first_name} #{owner.last_name} to a Group", 'Submit'
     end
 
-    it 'inviting from group page' do
-      sign_in(owner)
-      click_on 'Create a Group'
-      create_group(group)
-      click_on 'Create Group'
+    context 'with email' do
+      it 'inviting from group page' do
+        sign_in(owner)
+        click_on 'Create a Group'
+        create_group(group)
+        click_on 'Create Group'
 
-      fill_in 'email', with: invitee.email
-      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
-      click_on 'Submit'
+        fill_in 'Username or email', with: invitee.email
+        fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+        click_on 'Submit'
 
-      expect(page).to have_content 'Invitation sent.'
+        expect(page).to have_content 'Invitation sent.'
+      end
+
+      it 'inviting self from group page' do
+        sign_in(owner)
+        click_on 'Create a Group'
+        create_group(group)
+        click_on 'Create Group'
+
+        fill_in 'Username or email', with: owner.email
+        fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+        click_on 'Submit'
+
+        expect(page).to have_content "#{owner.first_name} #{owner.last_name}:"
+        expect(page).to have_content 'This user already has an invitation.'
+      end
     end
 
-    it 'inviting self from group page' do
-      sign_in(owner)
-      click_on 'Create a Group'
-      create_group(group)
-      click_on 'Create Group'
+    context 'with username' do
+      it 'inviting from group page' do
+        sign_in(owner)
+        click_on 'Create a Group'
+        create_group(group)
+        click_on 'Create Group'
 
-      fill_in 'email', with: owner.email
-      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
-      click_on 'Submit'
+        fill_in 'Username or email', with: invitee.username
+        fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+        click_on 'Submit'
 
-      expect(page).to have_content "#{owner.first_name} #{owner.last_name}:"
-      expect(page).to have_content "Invitation sent to user's email." # Failing conditional branch
+        expect(page).to have_content 'Invitation sent.'
+      end
+
+      it 'inviting self from group page' do
+        sign_in(owner)
+        click_on 'Create a Group'
+        create_group(group)
+        click_on 'Create Group'
+
+        fill_in 'Username or email', with: owner.username
+        fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+        click_on 'Submit'
+
+        expect(page).to have_content "#{owner.first_name} #{owner.last_name}:"
+        expect(page).to have_content 'This user already has an invitation.'
+      end
+
+      it 'incorrect username' do
+        sign_in(owner)
+        click_on 'Create a Group'
+        create_group(group)
+        click_on 'Create Group'
+
+        fill_in 'Username or email', with: invitee.username + 'whis'
+        fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+        click_on 'Submit'
+
+        expect(page).to have_content 'Invitation sent to user.'
+      end
     end
   end
 
   let(:group) { Group.create(name: 'Gift giving', description: 'We love gifts', gift_due_date: '2019-01-01') }
-  let(:owner) { User.create(first_name: 'Ray', last_name: 'Lee', username: 'LeeRay', email: 'email@ray.lee', password: 'hspa1203489y132809hss12034pas1203489y13280989y132809h') }
-  let(:invitee) { User.create!(first_name: 'Lee', last_name: 'Ray', username: 'RayLee', email: 'lee@ray.com', password: 'pas1203489y132809h132809hss1203489y132spa1203489y809h') }
+  let(:owner) { User.create(first_name: 'Ray', last_name: 'Lee', username: 'leeray', email: 'email@ray.lee', password: 'hspa1203489y132809hss12034pas1203489y13280989y132809h') }
+  let(:invitee) { User.create!(first_name: 'Lee', last_name: 'Ray', username: 'raylee', email: 'lee@ray.com', password: 'pas1203489y132809h132809hss1203489y132spa1203489y809h') }
 
   context 'not as a group owner' do
     it 'from user profile' do
@@ -70,20 +114,40 @@ describe 'creating an invitation' do
       expect(page).to have_no_content "Invite #{owner.first_name} #{owner.last_name} to a Group", 'Submit'
     end
 
-    it 'from group page' do
-      sign_in(owner)
-      click_on 'Create a Group'
-      create_group(group)
-      click_on 'Create Group'
-      fill_in 'email', with: invitee.email
-      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
-      click_on 'Submit'
-      expect(page).to have_content 'Invitation sent.'
-      sign_out
+    context 'from group page' do
+      it 'with email' do
+        sign_in(owner)
+        click_on 'Create a Group'
+        create_group(group)
+        click_on 'Create Group'
+        fill_in 'email', with: invitee.email
+        fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+        click_on 'Submit'
+        expect(page).to have_content 'Invitation sent.'
+        sign_out
 
-      sign_in(invitee)
-      click_on 'Accept'
-      expect(page).to have_no_content 'Send an invitation', 'Submit'
+        sign_in(invitee)
+        click_on 'Accept'
+        click_on group.name
+        expect(page).to have_no_content 'Send an invitation', 'Submit'
+      end
+
+      it 'with username' do
+       sign_in(owner)
+       click_on 'Create a Group'
+       create_group(group)
+       click_on 'Create Group'
+       fill_in 'email', with: invitee.username
+       fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+       click_on 'Submit'
+       expect(page).to have_content 'Invitation sent.'
+       sign_out
+
+       sign_in(invitee)
+       click_on 'Accept'
+       click_on group.name
+       expect(page).to have_no_content 'Send an invitation', 'Submit'
+     end
     end
   end
 end
--- spec/features/invitations/replying_to_invitation_spec.rb
+++ spec/features/invitations/replying_to_invitation_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'replying to an invitation' do
   let(:group) { Group.create(name: 'Gift giving', description: 'We love gifts', gift_due_date: '2019-01-01') }
-  let(:owner) { User.create(first_name: 'Ray', last_name: 'Lee', username: 'LeeRay', email: 'email@ray.lee', password: 'hspa1203489y132809hss12034pas1203489y13280989y132809h') }
-  let(:invitee) { User.create!(first_name: 'Lee', last_name: 'Ray', username: 'RayLee', email: 'lee@ray.com', password: 'pas1203489y132809h132809hss1203489y132spa1203489y809h') }
+  let(:owner) { User.create(first_name: 'Ray', last_name: 'Lee', username: 'leeray', email: 'email@ray.lee', password: 'hspa1203489y132809hss12034pas1203489y13280989y132809h') }
+  let(:invitee) { User.create!(first_name: 'Lee', last_name: 'Ray', username: 'raylee', email: 'lee@ray.com', password: 'pas1203489y132809h132809hss1203489y132spa1203489y809h') }
 
   it 'by accepting' do
     sign_in(owner)
@@ -24,15 +24,15 @@ describe 'replying to an invitation' do
     expect(page).to have_content 'Accept', 'Decline'
     click_on 'Accept'
 
-    expect(page).to have_content "Invitation accepted."
+    expect(page).to have_content 'Invitation accepted.'
     expect(page).to have_link group.name
     expect(page).to have_content group.description
     click_on group.name
 
-    # might change over time:
+    # might change over time
     expect(page).to have_content "#{owner.first_name} #{owner.last_name}: Profile Wish List (Group Owner)"
     expect(page).to have_content "#{invitee.first_name} #{invitee.last_name}:"
-    expect(page).to have_link "View your Wish List"
+    expect(page).to have_link 'View your Wish List'
   end
 
   it 'by declining' do
@@ -52,23 +52,23 @@ describe 'replying to an invitation' do
     expect(page).to have_content 'Accept', 'Decline'
     click_on 'Decline'
 
-    expect(page).to have_content "Invitation declined."
+    expect(page).to have_content 'Invitation declined.'
     expect(page).to have_no_content group.name
   end
 end
 
-describe 'replying to an invitation' do
-  let(:group) { Group.create(name: 'Gift giving', description: 'We love gifts', gift_due_date: '2019-01-01') }
-  let(:owner) { User.create(first_name: 'Ray', last_name: 'Lee', username: 'LeeRay', email: 'email@ray.lee', password: 'hspa1203489y132809hss12034pas1203489y13280989y132809h') }
-  let(:invitee) { User.create!(first_name: 'Lee', last_name: 'Ray', username: 'RayLee', email: 'lee@ray.com', password: 'pas1203489y132809h132809hss1203489y132spa1203489y809h') }
+describe 'owner sending multiple invites' do
+  let(:group) { Group.create(name: 'The season to give gifts', description: 'We love gifts more than you know', gift_due_date: '2022-01-01') }
+  let(:owner) { User.create(first_name: 'Leaf', last_name: 'Fall', username: 'fallleaf', email: 'email@fall.co', password: 'hspa1203489y132809hss12034pas1203489y13280989y132809h') }
+  let(:invitee) { User.create!(first_name: 'Punkin', last_name: 'Patch', username: 'october', email: 'punkin@october.patch', password: 'pas1203489y132809h132809hss1203489y132spa1203489y809h') }
 
-  context 'by declining' do
-    it 'and the owner submitting another invitation' do
+  context 'with email' do
+    it 'user declining' do
       sign_in(owner)
       click_on 'Create a Group'
       create_group(group)
       click_on 'Create Group'
-      fill_in 'email', with: invitee.email
+      fill_in 'Username or email', with: invitee.email
       fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
       click_on 'Submit'
       expect(page).to have_content 'Invitation sent.'
@@ -79,7 +79,73 @@ describe 'replying to an invitation' do
       expect(page).to have_content "#{owner.first_name} #{owner.last_name} has invited you to join the group #{group.name}!"
       expect(page).to have_content 'Accept', 'Decline'
       click_on 'Decline'
-      expect(page).to have_content "Invitation declined."
+      expect(page).to have_content 'Invitation declined.'
+      expect(page).to have_no_content group.name
+      sign_out
+
+      sign_in(owner)
+      click_on group.name
+      fill_in 'Username or email', with: invitee.email
+      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+      click_on 'Submit'
+      expect(page).to have_content 'This user already has an invitation.'
+
+      sign_out
+      sign_in(invitee)
+      expect(page).to have_no_content group.name
+    end
+
+    it 'user accepting' do
+      sign_in(owner)
+      click_on 'Create a Group'
+      create_group(group)
+      click_on 'Create Group'
+      fill_in 'Username or email', with: invitee.email
+      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+      click_on 'Submit'
+      expect(page).to have_content 'Invitation sent.'
+      sign_out
+
+      sign_in(invitee)
+      expect(current_path).to eq dashboard_path
+      expect(page).to have_content "#{owner.first_name} #{owner.last_name} has invited you to join the group #{group.name}!"
+      expect(page).to have_content 'Accept', 'Decline'
+      click_on 'Accept'
+      expect(page).to have_content 'Invitation accepted.'
+      expect(page).to have_content group.name
+      sign_out
+
+      sign_in(owner)
+      click_on group.name
+      fill_in 'Username or email', with: invitee.email
+      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+      click_on 'Submit'
+      expect(page).to have_content 'This user already has an invitation.'
+
+      sign_out
+      sign_in(invitee)
+      expect(page).to have_no_content "#{owner.first_name} #{owner.last_name} has invited you to join the group #{group.name}!"
+    end
+  end
+
+  context 'with username' do
+    it 'user declining' do
+      sign_in(owner)
+      click_on 'Create a Group'
+      create_group(group)
+      click_on 'Create Group'
+      fill_in 'Username or email', with: invitee.username
+      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+      click_on 'Submit'
+      expect(page).to have_content 'Invitation sent.'
+      sign_out
+
+      sign_in(invitee)
+      expect(current_path).to eq dashboard_path
+      expect(page).to have_content "#{owner.first_name} #{owner.last_name} has invited you to join the group #{group.name}!"
+      expect(page).to have_content 'Accept', 'Decline'
+      click_on 'Decline'
+      expect(page).to have_content 'Invitation declined.'
       expect(page).to have_no_content group.name
       sign_out
 
@@ -88,11 +154,44 @@ describe 'replying to an invitation' do
       fill_in 'email', with: invitee.email
       fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
       click_on 'Submit'
-      expect(page).to have_content "Invitation sent to user's email." # Failing conditional branch
+      expect(page).to have_content 'This user already has an invitation.'
 
       sign_out
       sign_in(invitee)
       expect(page).to have_no_content group.name
+    end
+
+    it 'user accepting' do
+      sign_in(owner)
+      click_on 'Create a Group'
+      create_group(group)
+      click_on 'Create Group'
+      fill_in 'email', with: invitee.username
+      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+      click_on 'Submit'
+      expect(page).to have_content 'Invitation sent.'
+      sign_out
+
+      sign_in(invitee)
+      expect(current_path).to eq dashboard_path
+      expect(page).to have_content "#{owner.first_name} #{owner.last_name} has invited you to join the group #{group.name}!"
+      expect(page).to have_content 'Accept', 'Decline'
+      click_on 'Accept'
+      expect(page).to have_content 'Invitation accepted.'
+      expect(page).to have_no_content "#{owner.first_name} #{owner.last_name} has invited you to join the group #{group.name}!"
+
+      sign_out
+
+      sign_in(owner)
+      click_on group.name
+      fill_in 'email', with: invitee.email
+      fill_in 'invitation[comment]', with: 'Would you like to join our great group?'
+      click_on 'Submit'
+      expect(page).to have_content 'This user already has an invitation.'
+
+      sign_out
+      sign_in(invitee)
+      expect(page).to have_no_content "#{owner.first_name} #{owner.last_name} has invited you to join the group #{group.name}!"
     end
   end
 end

bd213dd1c245987c5eeb364323b444c199d20e6e
Kurt Meyerhofer <k@kcmr.io>
2018-10-30 10:03:05 -0600

    User cannot have more than one invitation per group. A group owner cannot invite themselves.

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,4 +3,8 @@ class Invitation < ApplicationRecord
   belongs_to :user, foreign_key: :receiver_id
   belongs_to :group
   validates :comment, presence: true
+  validates_uniqueness_of :receiver_id, scope: :group_id,
+                    message: 'This user has already been invited to this group.'
+  validates_exclusion_of :sender_id, in: -> (invitation) { [invitation.receiver_id] },
+                          message: 'You cannot send an invitation to yourself.'
 end

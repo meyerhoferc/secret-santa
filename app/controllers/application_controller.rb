6948e83a09b098cdd70e9434f414ac30c21d3619
Kurt Meyerhofer <k@kcmr.io>
2018-10-30 14:29:39 -0600

    Adds redirection to root path unless a user is logged in, blocking access to the application.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :authorized_user
+  before_action :root_path_if_not_logged_in
 
   def current_user
    @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -7,5 +8,14 @@ class ApplicationController < ActionController::Base
 
   def authorized_user(other_user)
     current_user.id == other_user.id
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def root_path_if_not_logged_in
+    flash[:warning] = 'You need to be logged in first.' unless logged_in?
+    redirect_to root_path unless logged_in?
   end
 end

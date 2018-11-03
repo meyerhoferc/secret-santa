bc9384e3fd489acf5904c765074d8f0af6024b40
Kurt Meyerhofer <k@kcmr.io>
2018-11-03 14:22:20 -0600

    Adds validation error messages.

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,16 +3,17 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
   has_many :lists
   has_many :invitations
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, message: 'Choose a different group name, this name is already in use.'
   validates :name, presence: true
   validates :description, presence: true
   belongs_to :owner, foreign_key: :owner_id, class_name: 'User'
   validates :owner_id, presence: true
   validates :gift_due_date, presence: true
+  # validate gift due date is in the future only!
 
   def user_wish_list(user)
     list = self.lists.where(['user_id = :user_id AND group_id = :group_id',
                      { user_id: user.id, group_id: self.id }])
-    list[0]
+    list.first
   end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,7 +4,7 @@ class Invitation < ApplicationRecord
   belongs_to :group
   validates :comment, presence: true
   validates_uniqueness_of :receiver_id, scope: :group_id,
-                    message: 'This user has already been invited to this group.'
+                    message: 'has already been invited to this group'
   validates_exclusion_of :sender_id, in: -> (invitation) { [invitation.receiver_id] },
-                          message: 'You cannot send an invitation to yourself.'
+                          message: 'cannot send yourself an invitation'
 end

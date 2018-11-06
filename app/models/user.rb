4b465594d28bba37f79c42f4bbf7faef11b9e9ac
Kurt Meyerhofer <k@kcmr.io>
2018-11-05 18:34:07 -0700

    Modifies model associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
-
   has_many :user_groups
   has_many :groups, through: :user_groups
   has_many :lists
-  has_many :invitations
+  has_many :received, foreign_key: :receiver_id, class_name: 'Invitation'
+  has_many :sent, foreign_key: :sender_id, class_name: 'Invitation'
   validates_uniqueness_of :email
+  validates_uniqueness_of :id, scope: [:user_groups, :groups]
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
 
-  include ActiveModel::Validations
   has_secure_password
   validates :password, password_strength: {use_dictionary: true}
 end

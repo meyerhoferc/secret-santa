f0202c03fa00fa791910141a99ae477b9b77c1a4
Kurt Meyerhofer <k@kcmr.io>
2018-11-07 11:19:22 -0700

    Removes validation duplication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   has_many :received, foreign_key: :receiver_id, class_name: 'Invitation'
   has_many :sent, foreign_key: :sender_id, class_name: 'Invitation'
   validates_uniqueness_of :email
-  validates_uniqueness_of :id, scope: [:user_groups, :groups]
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true

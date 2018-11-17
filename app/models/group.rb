cc04daf77a18493495244cb0cbf94020a96fd01d
Kurt Meyerhofer <k@kcmr.io>
2018-11-16 21:37:56 -0700

    Adds method to return number of users in exclusion teams.

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,10 +5,10 @@ class Group < ApplicationRecord
   has_many :invitations
   has_many :exclusion_teams
   has_many :santa_assignments
-  validates :name, presence: true
-  validates_uniqueness_of :name, message: 'is already in use.'
-  validates :description, presence: true
   belongs_to :owner, foreign_key: :owner_id, class_name: 'User'
+  validates_uniqueness_of :name, message: 'is already in use.'
+  validates :name, presence: true
+  validates :description, presence: true
   validates :owner_id, presence: true
   validates :gift_due_date, presence: true
   # validate gift due date is in the future only, on update only!
@@ -20,7 +20,17 @@ class Group < ApplicationRecord
   end
 
   def no_exclusion_team_users
-    available_users = users.pluck(:user_id) - exclusion_teams.joins(:users).pluck(:user_id)
+    available_users = users.pluck(:user_id) - users_in_exclusion_teams
     User.find(available_users)
+  end
+
+  def user_exclusion_team_count
+    users_in_exclusion_teams.count
+  end
+
+  private
+
+  def users_in_exclusion_teams
+    exclusion_teams.joins(:users).pluck(:user_id)
   end
 end

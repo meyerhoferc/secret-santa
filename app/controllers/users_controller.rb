168d4cf431c51bc3a0f25f894295ee67eff80c74
kmeyerhofer <k@kcmr.io>
2018-11-16 20:04:33 -0700

    Adds first mailer files.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      NotifierMailer.welcome(@user).deliver_now
       redirect_to login_path
     else
       render 'new'

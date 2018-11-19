7332298426a9bac96b6e82fb60d00f966ab5dcfe
Kurt Meyerhofer <k@kcmr.io>
2018-11-19 11:51:47 -0700

    Matching algorithm tweaks.

--- app/services/santa_assignment_service.rb
+++ app/services/santa_assignment_service.rb
@@ -21,41 +21,62 @@ class SantaAssignmentService
   def santa_assignments!
     users = sorted_users
     if @group.exclusion_teams.any?
+      # byebug
       assignment_array = inverse_bubble_sort(users)
+      # byebug
+      if assignment_array.empty?
+        self.errors = 'Santa Assignments error.'
+        self.validity = false
+      else
+        self.validity = assignment_array
+      end
     else
 
     end
-    # byebug
   end
 
   def inverse_bubble_sort(array)
     limit = array.length - 1
-    loop do
-      break if array.size <= 1
+    iteration_limit = array.length * 50
+    failsafe = 0
+    while failsafe <= iteration_limit do # 50 iterations over the array
+      break if array.size <= 1 # Small array failsafe
       swap_count = 0
-      0.upto(limit - 1) do |int|
-        if int == 0 || int == limit - 1 # Ends of the array
+      0.upto(limit) do |int|
+        if int <= 0 # Beginning of the array
           if invalid_assignment?(array[int], array[int + 1])
             array[int], array[int + 1] = array[int + 1], array[int]
             swap_count += 1
           end
-        else # Middle of array
+        elsif int >= limit # End of the array
+          if invalid_assignment?(array[int - int], array[int]) || invalid_assignment?(array[int - 1], array[int]) # Loop back to beginning of array
+            array[int], array[int - int] = array[int - int], array[int]
+            swap_count += 1
+          end
+        elsif int >= 1 && int <= limit - 1 # Middle of array
           if invalid_assignment?(array[int], array[int + 1]) || invalid_assignment?(array[int - 1], array[int]) # Users before and after int
             array[int], array[int + 1] = array[int + 1], array[int]
             swap_count += 1
           end
         end
+        failsafe += 1
       end
-      break if swap_count == 0
+
+      break if swap_count == 0 # If no swaps have occurred in one iteration
+      array = [] if failsafe + 1 >= iteration_limit
     end
+    array
   end
 
   def invalid_assignment?(user_one, user_two)
-    User.find(user_one)
-      .exclusion_teams
-      .find_by(group_id: @group.id)
-      .users
-      .include?(User.find(user_two))
+    teams = User.find(user_one).exclusion_teams.find_by(group_id: @group.id)
+    if !teams.nil?
+      teams
+        .users
+        .include?(User.find(user_two))
+    else
+      false
+    end
   end
 
   def sorted_users
@@ -64,7 +85,7 @@ class SantaAssignmentService
       .sort_by { |team| team.users.count }
       .reverse
       .each { |team| user_id_array.push(team.users.pluck(:user_id)) }
-    user_id_array.push(@group.no_exclusion_team_users.pluck(:user_id))
+    user_id_array.push(@group.no_exclusion_team_users.pluck(:id))
     user_id_array.flatten!
   end
 

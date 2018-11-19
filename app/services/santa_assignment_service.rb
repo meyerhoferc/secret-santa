e2c739191c0175c70c5e027562f2fd9f35dbb2cd
Kurt Meyerhofer <k@kcmr.io>
2018-11-19 08:32:49 -0700

    Adds inverse bubble sort array.

--- app/services/santa_assignment_service.rb
+++ app/services/santa_assignment_service.rb
@@ -19,7 +19,53 @@ class SantaAssignmentService
   private
 
   def santa_assignments!
-    
+    users = sorted_users
+    if @group.exclusion_teams.any?
+      assignment_array = inverse_bubble_sort(users)
+    else
+
+    end
+    # byebug
+  end
+
+  def inverse_bubble_sort(array)
+    limit = array.length - 1
+    loop do
+      break if array.size <= 1
+      swap_count = 0
+      0.upto(limit - 1) do |int|
+        if int == 0 || int == limit - 1 # Ends of the array
+          if invalid_assignment?(array[int], array[int + 1])
+            array[int], array[int + 1] = array[int + 1], array[int]
+            swap_count += 1
+          end
+        else # Middle of array
+          if invalid_assignment?(array[int], array[int + 1]) || invalid_assignment?(array[int - 1], array[int]) # Users before and after int
+            array[int], array[int + 1] = array[int + 1], array[int]
+            swap_count += 1
+          end
+        end
+      end
+      break if swap_count == 0
+    end
+  end
+
+  def invalid_assignment?(user_one, user_two)
+    User.find(user_one)
+      .exclusion_teams
+      .find_by(group_id: @group.id)
+      .users
+      .include?(User.find(user_two))
+  end
+
+  def sorted_users
+    user_id_array = []
+    sorted_teams = @group.exclusion_teams
+      .sort_by { |team| team.users.count }
+      .reverse
+      .each { |team| user_id_array.push(team.users.pluck(:user_id)) }
+    user_id_array.push(@group.no_exclusion_team_users.pluck(:user_id))
+    user_id_array.flatten!
   end
 
   def validity=(value)

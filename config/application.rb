4f4072e8a89d6cb1810c6cb5fdc4457c78451fef
Kurt Meyerhofer <k@kcmr.io>
2019-05-02 21:18:29 -0600

    Adds api_only setting for future use, disabled currently.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 module SecretSanta
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.api_only = false
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.

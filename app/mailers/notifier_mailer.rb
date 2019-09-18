9ad3b287bf95b7a22d6283228b718978739d63c1
Kurt Meyerhofer <k@kcmr.io>
2019-09-18 00:07:08 -0600

    Adds beginnings of gift due date approaching email.

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,10 +1,15 @@
 class NotifierMailer < ApplicationMailer
-  default from: 'santa@kcmr.io'
-  
   def welcome
     @user = params[:user]
     email_with_name = %("#{@user.full_name}" <#{@user.email}>)
     @login_url = { controller: 'sessions', action: 'new' }
     mail(to: email_with_name, subject: 'Welcome to Secret Santa')
+  end
+
+  def gift_due_date(days)
+    @user = params[:user]
+    @group = params[:group]
+    email_with_name = %("#{@user.full_name}" <#{@user.email}>)
+    mail(to: email_with_name, subject: "In #{days.to_s} days your Secret Santa gift is due")
   end
 end

613cd821fcadabfc786bce9d2b51e816cc519cf1
Kurt Meyerhofer <k@kcmr.io>
2019-10-02 23:11:26 -0600

    Utilizes credentialed user.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "#{Rails.application.credentials.smtp_user}@kcmr.io"
+  default from: "#{Rails.application.credentials.smtp_user}"
   layout 'mailer'
 end

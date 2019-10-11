d25c40bef324cc6b2807358cee4319fc7ef2948b
Kurt Meyerhofer <k@kcmr.io>
2019-10-11 00:19:04 -0600

    Adds exception rescue emailer for rake task.

--- app/mailers/rescue_mailer.rb
+++ app/mailers/rescue_mailer.rb
@@ -1,7 +1,8 @@
 class RescueMailer < ApplicationMailer
-  def rake_task_rescue(task, error)
+  def rake_task_rescue(task, error_message, error_backtrace)
     @task = task
-    @error = error
-    mail(to: "#{Rails.application.credentials.admin_email}", subject: "Task #{task} has encountered an exception")
+    @error_message = error_message
+    @error_backtrace = error_backtrace
+    mail(to: "#{Rails.application.credentials.admin_email}", subject: "#{task} has encountered an exception")
   end
 end

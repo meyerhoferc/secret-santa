b64225e109056549ccc5f2f679720154af31e241
Kurt Meyerhofer <k@kcmr.io>
2019-12-05 20:12:18 -0700

    Refactors redirection. Utilizes deletable check method.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,12 @@
 class CommentsController < ApplicationController
-
   def create
-    comment = Comment.new(comment_params)
-    if comment.save
+    @comment = Comment.new(comment_params)
+    if @comment.save
       flash[:notice] = 'Comment added.'
     else
       flash[:warning] = 'Comment could not be saved.'
     end
-    if comment.commentable_type == 'Group'
-      redirect_to comment.commentable
-    elsif comment.commentable_type == 'List'
-      redirect_to group_list_path(comment.commentable.group_id, comment.commentable.id)
-    elsif comment.commentable_type == 'Item'
-      redirect_to group_list_item_path(comment.commentable.list.group_id, comment.commentable.list_id, comment.commentable.id)
-    end
+    redirect_to redirect_path
   end
 
   def edit
@@ -25,25 +18,29 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    comment = Comment.find(params[:id])
-    if current_user.id == comment.user_id
+    @comment = Comment.find(params[:id])
+    if @comment.deletable_by_user_id?(current_user.id)
       flash[:notice] = 'Comment deleted.'
-      comment.delete
+      @comment.delete
     else
       flash[:warning] = 'You cannot delete this comment.'
     end
-    if comment.commentable_type == 'Group'
-      redirect_to comment.commentable
-    elsif comment.commentable_type == 'List'
-      redirect_to group_list_path(comment.commentable.group_id, comment.commentable.id)
-    elsif comment.commentable_type == 'Item'
-      redirect_to group_list_item_path(comment.commentable.list.group_id, comment.commentable.list_id, comment.commentable.id)
-    end
+    redirect_to redirect_path
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:user_id, :commentable_id, :commentable_type, :text)
+  end
+
+  def redirect_path
+    if @comment.commentable_type == 'Group'
+      @comment.commentable
+    elsif @comment.commentable_type == 'List'
+      group_list_path(@comment.commentable.group_id, @comment.commentable.id)
+    elsif @comment.commentable_type == 'Item'
+      group_list_item_path(@comment.commentable.list.group_id, @comment.commentable.list_id, @comment.commentable.id)
+    end
   end
 end

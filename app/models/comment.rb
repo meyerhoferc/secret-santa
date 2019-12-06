d97898598b16b5b3c315d14c62db844b9995b357
Kurt Meyerhofer <k@kcmr.io>
2019-12-05 20:11:49 -0700

    Adds deletable check method.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,19 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  def deletable_by_user_id?(user_id)
+    if self.user_id == user.id
+      true
+    else
+      if commentable_type == 'Group'
+        group = commentable
+      elsif commentable_type == 'List'
+        group = commentable.group
+      elsif commentable_type == 'Item'
+        group = commentable.list.group
+      end
+      user_id == group.owner_id
+    end        
+  end
 end
